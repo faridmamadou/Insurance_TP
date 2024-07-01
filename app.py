35ed35d872e74812173064dd8cf37a9bed7d3ae8
faridmamadou <136201709+faridmamadou@users.noreply.github.com>
2024-07-01 23:16:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,15 @@ df = pd.DataFrame(data)
 # Encodage des variables catégorielles
 df['sex'] = df['sex'].map({'male': 1, 'female': 0})
 df['smoker'] = df['smoker'].map({'yes': 1, 'no': 0})
-df = pd.get_dummies(df, columns=['region'], drop_first=True)
+df = pd.get_dummies(df, columns=['region'])
+
+# Ajout des colonnes manquantes avec des zéros
+for col in ['region_northeast', 'region_northwest', 'region_southeast', 'region_southwest']:
+    if col not in df.columns:
+        df[col] = 0
+
+# Réordonnancer les colonnes pour correspondre à celles utilisées lors de l'entraînement
+df = df[['age', 'sex', 'bmi', 'children', 'smoker', 'region_northeast', 'region_northwest', 'region_southeast', 'region_southwest']]
 
 # Normalisation des variables quantitatives
 df_normalized = normalize_data(df, scaler)
